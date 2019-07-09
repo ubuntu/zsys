4106fcf3c5359d8cabe1c00127bb477937b088e8
Didier Roche <didrocks@ubuntu.com>
2019-07-09 16:19:20 +0200

    Set debug log level when printing verbose mode

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,25 @@
 package config
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 // ErrorFormat switch between "%v" and "%+v" depending if we want more verbose info
 var ErrorFormat = "%v"
 
-// SetVerboseMode change ErrorFormat between verbose and non verbose mode
+// SetVerboseMode change ErrorFormat and logs between verbose and non verbose mode
 func SetVerboseMode(verbose bool) {
 	if verbose {
 		ErrorFormat = "%+v"
+		log.SetFormatter(&log.TextFormatter{DisableLevelTruncation: true})
+		log.SetLevel(log.DebugLevel)
+		log.Debug("verbosity set to debug and will print stacktraces")
 	} else {
 		ErrorFormat = "%v"
+		log.SetFormatter(&log.TextFormatter{
+			DisableLevelTruncation: true,
+			DisableTimestamp:       true,
+		})
+		log.SetLevel(log.WarnLevel)
 	}
 }
