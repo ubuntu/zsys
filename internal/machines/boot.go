93dd16b79363fa3c93ebb6658399cb88a786869f
Didier Roche <didrocks@ubuntu.com>
2019-07-12 10:52:35 +0200

    More correct EnsureBoot implementation
    EnsureBoot now handle cloning and user datasets, while rescanning when
    needed.
    It clones on snapshots boot system dataset and creates user datasets if
    we want to revert user data.
    Then, it pick the correct user data if we requested a revert (on
    snapshot or existing clone).
    The only other zfs modifications it does is to set canmount noauto/on when
    needed (This could be probably optimized with a cache later).
    Implements helper modifications and ensure we return a scanned, current
    value, with the expected current state and machine.

--- internal/machines/boot.go
+++ internal/machines/boot.go
@@ -1,6 +1,11 @@
 package machines
 
 import (
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/ubuntu/zsys/internal/config"
 	"github.com/ubuntu/zsys/internal/zfs"
 	"golang.org/x/xerrors"
@@ -8,51 +13,194 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// ZfsPropertySetter interface tries to set dataset property on a zfs system
-type ZfsPropertySetter interface {
+// ZfsPropertyCloneScanner interface can clone, set dataset property and scan
+type ZfsPropertyCloneScanner interface {
+	Clone(name, suffix string, skipBootfs, recursive bool, options ...zfs.DatasetCloneOption) (errClone error)
+	Scan() ([]zfs.Dataset, error)
+	zfsPropertySetter
+}
+
+// zfsPropertySetter can only SetProperty
+type zfsPropertySetter interface {
 	SetProperty(name, value, datasetName string, force bool) error
 }
 
-// EnsureBoot consolidate canmount states for early boot.
+// EnsureBoot consolidates canmount states for early boot.
+// It will create any needed clones and userdata if required.
 // A transactional zfs element should be passed to optionally revert if an error is returned (only part of the datasets
 // were changed).
-// A new machines element should be built afterwards to ensure that datasets attached to State have the correct
-// canmount state.
+// We ensure that we don't modify any existing tags (those will be done in commit()) so that failing boots didn't modify
+// the system, apart for canmount auto/on which are consolidated unconditionnally on each boot anyway.
+// Note that a rescan if performed if any modifications change the dataset layout.
 // TODO: propagate error to user
-func (machines Machines) EnsureBoot(z ZfsPropertySetter) error {
+func (machines *Machines) EnsureBoot(z ZfsPropertyCloneScanner, cmdline string) error {
 	if !machines.current.IsZsys {
 		log.Debugln("current machine isn't Zsys, nothing to do")
 		return nil
 	}
 
-	// Start switching every non current machine to noauto
+	root, revertUserData := parseCmdLine(cmdline)
+	m, bootedState := machines.findFromRoot(root)
+
+	bootedOnSnapshot := hasBootedOnSnapshot(cmdline)
+	// We are creating new clones (bootfs and optionnally, userdata)
+	if bootedOnSnapshot {
+		// get current generated suffix by initramfs
+		var suffix string
+		if j := strings.LastIndex(bootedState.ID, "_"); j > 0 && !strings.HasSuffix(bootedState.ID, "_") {
+			suffix = bootedState.ID[j+1:]
+		} else {
+			return xerrors.Errorf("Mounted clone bootFS dataset created by initramfs doesn't have a valid _suffix (at least .*_<onechar>): %q", s.ID)
+		}
+
+		if err := z.Clone(root, suffix, true, true); err != nil {
+			return xerrors.Errorf("couldn't create new subdatasets from %q: %v", root, err)
+		}
+
+		// Handle userdata by creating a new clone in case a revert was requested
+		var userDataSuffix string
+		if revertUserData {
+			// Find user datasets attached to the snapshot and clone them
+			userDataSuffix = generateID(6)
+			snapshot := m.History[root]
+			for _, d := range snapshot.UserDatasets {
+				// Don't do recursive cloning. The datasets are ordered, but not necessarily child of each other.
+				// We will tag them at the same time.
+				if err := z.Clone(d.Name, userDataSuffix, false, true); err != nil {
+					return xerrors.Errorf("couldn't create new user datasets from %q: %v", root, err)
+				}
+			}
+		}
+
+		// Rescan here for getting accessing to new cloned datasets
+		ds, err := z.Scan()
+		if err != nil {
+			return xerrors.Errorf("couldn't rescan after modifying boot: "+config.ErrorFormat, err)
+		}
+		*machines = New(ds, cmdline)
+
+		// reassociate newly cloned userdata to state (the userDatasets liste is empty as those were not tagged yet)
+		if revertUserData {
+			m, bootedState = machines.findFromRoot(root) // We did rescan, refresh pointers
+			var newUserDatasets []zfs.Dataset
+			for _, d := range ds {
+				if !strings.Contains(strings.ToLower(d.Name), userdatasetsContainerName) {
+					continue
+				}
+				if strings.HasSuffix(d.Name, "_"+userDataSuffix) || strings.Contains(d.Name, "_"+userDataSuffix+"/") {
+					newUserDatasets = append(newUserDatasets, d)
+					continue
+				}
+			}
+			bootedState.UserDatasets = newUserDatasets
+		}
+	}
+
+	// 2 cases:
+	// * we asked to revert user datasets, and so, s.UserDatasets will be what we requested and should switch canmount=on -> do nothing thus.
+	// * if we keep current user datasets, take the main State UserDatasets from the current machine (which was the last successfully booted with ones)
+	// Save them for switching to noauto if needed
+	var bootedStateGeneratedUserDatasets []zfs.Dataset
+	if !revertUserData {
+		bootedStateGeneratedUserDatasets = bootedState.UserDatasets
+		bootedState.UserDatasets = m.UserDatasets
+	}
+
+	var needRescan bool
+	// Start switching every non desired states to noauto
 	for _, m := range machines.all {
-		if m != machines.current {
-			if err := m.switchCanMount(z, "noauto"); err != nil {
+		if m.ID != bootedState.ID {
+			modified, err := m.switchCanMount(z, "noauto")
+			if err != nil {
 				return err
+			}
+			if modified {
+				needRescan = modified
 			}
 		}
 		for _, h := range m.History {
-			if err := h.switchCanMount(z, "noauto"); err != nil {
+			// skipping current state
+			if h.ID == bootedState.ID {
+				continue
+			}
+			modified, err := h.switchCanMount(z, "noauto")
+			if err != nil {
 				return err
+			}
+			if modified {
+				needRescan = modified
+			}
+		}
+	}
+	// If we reverted usersdataset, the boot failed (but those user datasets have canmount=on),
+	// and then reboot on same dataset but without reverting userdataset, we need to disable the previously
+	// associated userdatasets.
+	if !revertUserData && bootedStateGeneratedUserDatasets != nil {
+		for _, d := range bootedStateGeneratedUserDatasets {
+			if d.CanMount == "noauto" {
+				continue
+			}
+			needRescan = true
+			if err := z.SetProperty(zfs.CanmountProp, "noauto", d.Name, false); err != nil {
+				return xerrors.Errorf("couldn't switch %q canmount property to noauto: "+config.ErrorFormat, d.Name, err)
 			}
 		}
 	}
 
 	// Switch current machine to on (that way, overlapping userdataset will have the correct state)
-	return machines.current.switchCanMount(z, "on")
+	modified, err := bootedState.switchCanMount(z, "on")
+	if err != nil {
+		return err
+	}
+	if modified {
+		needRescan = modified
+	}
+
+	// Rescan if we changed the dataset properties
+	if needRescan {
+		ds, err := z.Scan()
+		if err != nil {
+			return xerrors.Errorf("couldn't rescan after modifying boot: "+config.ErrorFormat, err)
+		}
+		*machines = New(ds, cmdline)
+	}
+
+	// Change the state to set current state as main machine state may have changed
+	if bootedOnSnapshot {
+		machines.ensureCurrentState(cmdline)
+	}
+
+	return nil
 }
 
-// switchCanMount switches for a given state all system and user datasets to canMount state
-func (s *State) switchCanMount(z ZfsPropertySetter, canMount string) error {
+var seedOnce = sync.Once{}
+
+// generateID with n ascii or digits, lowercase, characters
+func generateID(n int) string {
+	seedOnce.Do(func() { rand.Seed(time.Now().UnixNano()) })
+
+	var allowedRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = allowedRunes[rand.Intn(len(allowedRunes))]
+	}
+	return string(b)
+}
+
+// switchCanMount switches for a given state all system and user datasets to canMount state.
+// It returns if we had to modify any dataset.
+func (s *State) switchCanMount(z zfsPropertySetter, canMount string) (bool, error) {
+	var modified bool
 	ds := append(s.SystemDatasets, s.UserDatasets...)
 	for _, d := range ds {
 		if d.CanMount == canMount {
 			continue
 		}
+		modified = true
 		if err := z.SetProperty(zfs.CanmountProp, canMount, d.Name, false); err != nil {
-			return xerrors.Errorf("couldn't switch %q canmount property to %d: "+config.ErrorFormat, d.Name, canMount, err)
+			return modified, xerrors.Errorf("couldn't switch %q canmount property to %d: "+config.ErrorFormat, d.Name, canMount, err)
 		}
 	}
-	return nil
+	return modified, nil
 }
--- internal/machines/cmdline.go
+++ internal/machines/cmdline.go
@@ -22,3 +22,31 @@ func parseCmdLine(cmdline string) (rootDataset string, revertuserData bool) {
 
 	return rootDataset, revertuserData
 }
+
+// ensureCurrentState resets the current machine state which is incorrect in case of snapshot
+// before we promote (via m.Commit()) it.
+func (machines *Machines) ensureCurrentState(cmdline string) {
+	if !hasBootedOnSnapshot(cmdline) {
+		return
+	}
+
+	root, _ := parseCmdLine(cmdline)
+	m, bootedState := machines.findFromRoot(root)
+	if m.ID == bootedState.ID {
+		return
+	}
+
+	// switch between history state and main machine state dataset
+	m.History[m.ID] = &m.State
+	m.State = *bootedState
+	delete(m.History, bootedState.ID)
+	// switch now main index object, as main machine state dataset has changed
+	machines.all[bootedState.ID] = m
+	delete(machines.all, m.ID)
+}
+
+// hasBootedOnSnapshot returns if we booted on a snapshot
+func hasBootedOnSnapshot(cmdline string) bool {
+	root, _ := parseCmdLine(cmdline)
+	return strings.Contains(root, "@")
+}
