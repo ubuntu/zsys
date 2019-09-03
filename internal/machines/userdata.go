c89e24451fcb9390827ee09fbb38a4c514aea790
Didier Roche <didrocks@ubuntu.com>
2019-09-03 11:37:16 +0200

    Split out trying to match an existing dataset code
    We'll reuse this function to match an existing dataset with old home as
    well for home directory renaming.

--- internal/machines/userdata.go
+++ internal/machines/userdata.go
@@ -21,6 +21,12 @@ type ZfsSetPropertyScanCreater interface {
 	zfsPropertySetter
 }
 
+// zfsSetPropertyScanner can only SetProperty and Scan
+type zfsSetPropertyScanner interface {
+	zfsScanner
+	zfsPropertySetter
+}
+
 // CreateUserData creates a new dataset for homepath and attach to current system.
 // It creates intermediates user datasets if needed.
 func (ms *Machines) CreateUserData(user, homepath string, z ZfsSetPropertyScanCreater) error {
@@ -35,33 +41,11 @@ func (ms *Machines) CreateUserData(user, homepath string, z ZfsSetPropertyScanCr
 		return errors.New("Needs a valid home path, got nothing")
 	}
 	// If there this user already attached to this machine: retarget home
-	for _, d := range ms.current.UserDatasets {
-		// get user name from dataset
-		n := strings.Split(d.Name, "/")
-		userName := n[len(n)-1]
-		n = strings.Split(userName, "_")
-		userName = strings.Join(n[0:len(n)-1], "_")
-
-		// Home path is already attached to current system, but with a different user name. Fail
-		if d.Mountpoint == homepath && user != userName {
-			return xerrors.Errorf("%q is already associated to %q, which is for a different user name (%q) than %q", homepath, d.Name, userName, user)
-		}
-
-		// We'll reuse that dataset
-		if userName == user {
-			log.Infof("Reusing %q as matching user name", d.Name)
-			if err := z.SetProperty(zfs.MountPointProp, homepath, d.Name, false); err != nil {
-				return xerrors.Errorf("couldn't set new home %q to %q: "+config.ErrorFormat, homepath, d.Name, err)
-			}
-			ds, err := z.Scan()
-			if err != nil {
-				// don't fail if Scan is failing, as the dataset was created
-				log.Warningf("couldn't rescan after committing boot: "+config.ErrorFormat, err)
-			} else {
-				*ms = New(ds, ms.cmdline)
-			}
-			return nil
-		}
+	reused, err := ms.tryReuseUserDataSet(user, "", homepath, z)
+	if err != nil {
+		return err
+	} else if reused {
+		return nil
 	}
 
 	log.Infof("create user dataset for %q\n", homepath)
@@ -140,4 +124,54 @@ func getUserDatasetPath(path string) string {
 		return ""
 	}
 	return path[0 : i+len(userdatasetsContainerName)]
+}
+
+// tryReuseUserDataSet tries to match an existing user dataset for the current machine.
+// user match is used first, if empty, it will try to match old home directory.
+func (ms *Machines) tryReuseUserDataSet(user string, oldhome, newhome string, z zfsSetPropertyScanner) (bool, error) {
+	log.Debugf("trying to check if there is a user or home directory already attached to this machine")
+
+	// If there this user or home already attached to this machine: retarget home
+	for _, d := range ms.current.UserDatasets {
+
+		var match bool
+		// try handling user dataset
+		if user != "" {
+			// get user name from dataset
+			n := strings.Split(d.Name, "/")
+			userName := n[len(n)-1]
+			n = strings.Split(userName, "_")
+			userName = strings.Join(n[0:len(n)-1], "_")
+
+			// Home path is already attached to current system, but with a different user name. Fail
+			if d.Mountpoint == newhome && user != userName {
+				return false, xerrors.Errorf("%q is already associated to %q, which is for a different user name (%q) than %q", newhome, d.Name, userName, user)
+			}
+			if userName == user {
+				match = true
+			}
+		} else if oldhome != "" {
+			if d.Mountpoint == oldhome {
+				match = true
+			}
+		}
+
+		// We'll reuse that dataset
+		if match {
+			log.Infof("Reusing %q as matching user name or old mountpoint", d.Name)
+			if err := z.SetProperty(zfs.MountPointProp, newhome, d.Name, false); err != nil {
+				return false, xerrors.Errorf("couldn't set new home %q to %q: "+config.ErrorFormat, newhome, d.Name, err)
+			}
+			ds, err := z.Scan()
+			if err != nil {
+				// don't fail if Scan is failing, as the dataset was created
+				log.Warningf("couldn't rescan after committing boot: "+config.ErrorFormat, err)
+			} else {
+				*ms = New(ds, ms.cmdline)
+			}
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
