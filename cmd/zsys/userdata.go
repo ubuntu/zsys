c2bc172cbeef7d74ffcaf7979d524f8400431f61
Didier Roche <didrocks@ubuntu.com>
2019-09-04 08:14:46 +0200

    Add "userdata" subcommand
    This hidden subcommand is used by useradd and usermod for adding and
    changing home directory.
    Each ones are implemented in a sub-subcommand hooked up similarly than
    the boot ones.

---
+++ cmd/zsys/userdata.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/ubuntu/zsys/internal/config"
+	"github.com/ubuntu/zsys/internal/zfs"
+	"golang.org/x/xerrors"
+)
+
+var (
+	userdataCmd = &cobra.Command{
+		Use:    "userdata COMMAND",
+		Short:  "User datasets creation and renaming",
+		Hidden: true,
+		RunE:   requireSubcommand,
+	}
+	userdataCreateCmd = &cobra.Command{
+		Use:   "create USER HOME_DIRECTORY",
+		Short: "Create a new home user dataset via an user dataset (if doesn't exist) creation",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdErr = createUserData(args[0], args[1])
+		},
+	}
+	userdataRenameCmd = &cobra.Command{
+		Use:   "set-home OLD_HOME NEW_HOME",
+		Short: "Rename a user's home directory via renaming corresponding user dataset",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdErr = changeHomeOnUserData(args[0], args[1])
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(userdataCmd)
+	userdataCmd.AddCommand(userdataCreateCmd)
+	userdataCmd.AddCommand(userdataRenameCmd)
+}
+
+// createUserData creates a new userdata for user and set it to homepath on current zsys system.
+// if the user already exists for a dataset attached to the current system, set its mountpoint to homepath.
+func createUserData(user, homepath string) (err error) {
+	ms, err := getMachines(zfs.New())
+	if err != nil {
+		return err
+	}
+
+	z := zfs.New(zfs.WithTransactions())
+	defer func() {
+		if err != nil {
+			z.Cancel()
+			err = xerrors.Errorf("couldn't create userdataset for %q: "+config.ErrorFormat, homepath, err)
+		} else {
+			z.Done()
+		}
+	}()
+
+	return ms.CreateUserData(user, homepath, z)
+}
+
+// changeHomeOnUserData change from home to newHome on current zsys system.
+func changeHomeOnUserData(home, newHome string) (err error) {
+	ms, err := getMachines(zfs.New())
+	if err != nil {
+		return err
+	}
+
+	z := zfs.New(zfs.WithTransactions())
+	defer func() {
+		if err != nil {
+			z.Cancel()
+			err = xerrors.Errorf("couldn't change home userdataset for %q: "+config.ErrorFormat, home, err)
+		} else {
+			z.Done()
+		}
+	}()
+
+	return ms.ChangeHomeOnUserData(home, newHome, z)
+}
