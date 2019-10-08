d41f68a0458f078dcdc05a53de988f841a52f68a
Didier Roche <didrocks@ubuntu.com>
2019-10-08 17:21:42 +0200

    Log package to proxy logs
    Package log proxy logs to logrus logger and an optional io.Writer.
    Both can have independent logging levels.

---
+++ internal/log/create.go
@@ -0,0 +1,66 @@
+package log
+
+import (
+	"context"
+	"crypto/rand"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	// DefaultLevel only prints warning and errors.
+	DefaultLevel = logrus.WarnLevel
+	// InfoLevel is signaling system information like global calls.
+	InfoLevel = logrus.InfoLevel
+	// DebugLevel gives fine-grained details about executions.
+	DebugLevel = logrus.DebugLevel
+)
+
+const (
+	defaultRequestID = "unknown"
+)
+
+// ContextWithLogger returns a context which will log to the writer.
+// Level is based on metadata information from the ctx request.
+// A generated request ID is added to a requester ID and attached to the context
+func ContextWithLogger(ctx context.Context, requesterID, level string, w io.Writer) (newCtx context.Context, err error) {
+	// Generate a request id.
+	requestID := defaultRequestID
+	b := make([]byte, 4)
+	if _, err := rand.Read(b); err != nil {
+		logrus.Warningf("couldn't generate request id, setting to %q: %v", defaultRequestID, err)
+	} else {
+		requestID = fmt.Sprintf("%x", b[0:])
+	}
+	id := fmt.Sprintf("%s:%s", requesterID, requestID)
+
+	// Get logging level.
+	logLevel := DefaultLevel
+	if logLevel, err = logrus.ParseLevel(level); err != nil {
+		logrus.Warningf("invalid log level requested. Using default: %v", err)
+	}
+
+	// Associate the context with a new logger, for which output is the io.Writer.
+	logger := logrus.New()
+	logger.SetOutput(w)
+	// ignore the TTY check in logrus and force color mode and not systemd printing format.
+	setLevelLogger(logger, logLevel, true)
+
+	return context.WithValue(ctx, requestInfoKey, &requestInfo{
+		id:     id,
+		logger: logger,
+	}), nil
+}
+
+// IDFromContext returns current request log id from context
+func IDFromContext(ctx context.Context) (string, error) {
+	info, ok := ctx.Value(requestInfoKey).(*requestInfo)
+	if !ok {
+		return "", errors.New("no request ID attached to this context")
+	}
+
+	return info.id, nil
+}
---
+++ internal/log/log.go
@@ -0,0 +1,114 @@
+/*
+
+Package log proxy logs to logrus logger and an optional io.Writer.
+Both can have independent logging levels.
+
+*/
+package log
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type requestInfoKeyType string
+
+const (
+	requestInfoKey requestInfoKeyType = "logrequestinfo"
+	reqIDFormat                       = "[[%s]] %s"
+)
+
+type requestInfo struct {
+	id     string
+	logger *logrus.Logger
+}
+
+// SetLevel sets default logger
+func SetLevel(l logrus.Level) {
+	setLevelLogger(logrus.StandardLogger(), l, false)
+}
+
+// setLevelLogger sets given logger to level.
+// "simplified" enables to ignore the TTY check in logrus and force color mode and not systemd printing.
+func setLevelLogger(logger *logrus.Logger, l logrus.Level, simplified bool) {
+	f := &logrus.TextFormatter{
+		DisableLevelTruncation: true,
+		DisableTimestamp:       true,
+	}
+	if simplified {
+		f.ForceColors = true
+	}
+
+	logger.SetLevel(l)
+	logger.SetFormatter(f)
+}
+
+// Debug logs a message at level Debug on the standard logger
+// and may push to the stream referenced by ctx.
+func Debug(ctx context.Context, args ...interface{}) {
+	Debugf(ctx, "%s", args...)
+}
+
+// Debugf logs a message at level Debug on the standard logger
+// and may push to the stream may push to the stream referenced by ctx.
+func Debugf(ctx context.Context, format string, args ...interface{}) {
+	if info, ok := ctx.Value(requestInfoKey).(*requestInfo); ok {
+		info.logger.Debugf(format, args...)
+		// for standard logger, save the id
+		format = fmt.Sprintf(reqIDFormat, info.id, format)
+	}
+	logrus.Debugf(format, args...)
+}
+
+// Info logs a message at level Info on the standard logger
+// and may push to the stream referenced by ctx.
+func Info(ctx context.Context, args ...interface{}) {
+	Infof(ctx, "%s", args...)
+}
+
+// Infof logs a message at level Info on the standard logger
+// and may push to the stream may push to the stream referenced by ctx.
+func Infof(ctx context.Context, format string, args ...interface{}) {
+	if info, ok := ctx.Value(requestInfoKey).(*requestInfo); ok {
+		info.logger.Infof(format, args...)
+		// for standard logger, save the id
+		format = fmt.Sprintf(reqIDFormat, info.id, format)
+	}
+	logrus.Infof(format, args...)
+}
+
+// Warning logs a message at level Warning on the standard logger
+// and may push to the stream referenced by ctx.
+func Warning(ctx context.Context, args ...interface{}) {
+	Warningf(ctx, "%s", args...)
+}
+
+// Warningf logs a message at level Warning on the standard logger
+// and may push to the stream may push to the stream referenced by ctx.
+func Warningf(ctx context.Context, format string, args ...interface{}) {
+	if info, ok := ctx.Value(requestInfoKey).(*requestInfo); ok {
+		info.logger.Warningf(format, args...)
+		// for standard logger, save the id
+		format = fmt.Sprintf(reqIDFormat, info.id, format)
+	}
+	logrus.Warningf(format, args...)
+}
+
+// Error logs a message at level Error on the standard logger
+// and may push to the stream referenced by ctx.
+func Error(ctx context.Context, args ...interface{}) {
+	Errorf(ctx, "%s", args...)
+}
+
+// Errorf logs a message at level Error on the standard logger
+// and may push to the stream may push to the stream referenced by ctx.
+func Errorf(ctx context.Context, format string, args ...interface{}) {
+	if info, ok := ctx.Value(requestInfoKey).(*requestInfo); ok {
+		info.logger.Errorf(format, args...)
+		// for standard logger, save the id
+		format = fmt.Sprintf(reqIDFormat, info.id, format)
+	}
+	logrus.Errorf(format, args...)
+}
