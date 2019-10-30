bb14241795bcf2a78a784cccd9204112c0b8c5d2
Didier Roche <didrocks@ubuntu.com>
2019-10-30 10:06:16 +0100

    Handle man page, bash completion and README generation
    We generate those using cobra handlers. Note that:
    * we needed to fork the bash completion mechanism to include our fixes
      for hidden commands and file completion to match
      github.com/didrocks/cobra branch-for-completion branch.
      The goal is to fix the completion issues addressed in
      https://github.com/spf13/cobra/pull/983 and file completion if no match
      was found.
      A detailed upstream bug is available at
      https://github.com/spf13/cobra/issues/981.
    * we post-process the markdown generation to skip SEE Also and other
      irrelevant sections and add subindentation.

---
+++ cmd/zsys/generate-mancomp.go
@@ -0,0 +1,237 @@
+// +build ignore
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+	"github.com/ubuntu/zsys/cmd/zsys/client"
+	"github.com/ubuntu/zsys/cmd/zsys/daemon"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		log.Fatal("Need one argument: output directory")
+	}
+
+	dir := os.Args[1]
+	if err := os.RemoveAll(dir); err != nil {
+		log.Fatalf("Couldn't delete out directory: %v", err)
+	}
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatalf("Couldn't create out directory: %v", err)
+	}
+
+	commands := []*cobra.Command{client.Cmd(), daemon.Cmd()}
+	genBashCompletions(commands, dir)
+	genManPages(commands, dir)
+	updateREADME(commands)
+}
+
+func genBashCompletions(cmds []*cobra.Command, dir string) {
+	out := filepath.Join(dir, "bash-completion")
+	if err := os.MkdirAll(out, 0755); err != nil {
+		log.Fatalf("Couldn't create bash completion directory: %v", err)
+	}
+
+	for _, cmd := range cmds {
+		if err := genBashCompletionFile(cmd, filepath.Join(out, cmd.Name())); err != nil {
+			log.Fatalf("Couldn't create bash completion for %s: %v", cmd.Name(), err)
+		}
+	}
+}
+
+func genManPages(cmds []*cobra.Command, dir string) {
+	out := filepath.Join(dir, "man")
+	if err := os.MkdirAll(out, 0755); err != nil {
+		log.Fatalf("Couldn't create man pages directory: %v", err)
+	}
+
+	for _, cmd := range cmds {
+		if err := genManTreeFromOpts(cmd, doc.GenManHeader{
+			Title: fmt.Sprintf("ZSYS: %s", cmd.Name()),
+		}, out); err != nil {
+			log.Fatalf("Couldn't generate man pages for %s: %v", cmd.Name(), err)
+		}
+	}
+}
+
+func updateREADME(cmds []*cobra.Command) {
+	_, current, _, ok := runtime.Caller(1)
+	if !ok {
+		log.Fatal("Couldn't find current file name")
+	}
+
+	readme := filepath.Join(filepath.Dir(current), "..", "..", "README.md")
+
+	in, err := os.Open(readme)
+	if err != nil {
+		log.Fatalf("Couldn't open source readme file: %v", err)
+	}
+	defer in.Close()
+
+	tmp, err := os.Create(readme + ".new")
+	if err != nil {
+		log.Fatalf("Couldn't create temporary readme file: %v", err)
+	}
+	defer tmp.Close()
+
+	src := bufio.NewScanner(in)
+	// Write header
+	for src.Scan() {
+		mustWriteLine(tmp, src.Text())
+		if src.Text() == "## Usage" {
+			mustWriteLine(tmp, "")
+			break
+		}
+	}
+	if err := src.Err(); err != nil {
+		log.Fatalf("Error when scanning source readme file: %v", err)
+	}
+
+	// Write markdown
+	user, system := getCmdsAndSystems(cmds)
+	mustWriteLine(tmp, "### User commands\n")
+	filterCommandMarkdown(user, tmp)
+	mustWriteLine(tmp, "### System commands\n")
+	mustWriteLine(tmp, "Those commands are hidden from help and should primarly be used by the system itself.\n")
+	filterCommandMarkdown(system, tmp)
+
+	// Write footer (skip previously generated content)
+	skip := true
+	for src.Scan() {
+		if strings.HasPrefix(src.Text(), "## ") {
+			skip = false
+		}
+		if skip {
+			continue
+		}
+
+		mustWriteLine(tmp, src.Text())
+	}
+	if err := src.Err(); err != nil {
+		log.Fatalf("Error when scanning source readme file: %v", err)
+	}
+
+	if err := in.Close(); err != nil {
+		log.Fatalf("Couldn't close source Rreadme file: %v", err)
+	}
+	if err := tmp.Close(); err != nil {
+		log.Fatalf("Couldn't close temporary readme file: %v", err)
+	}
+	if err := os.Rename(readme+".new", readme); err != nil {
+		log.Fatalf("Couldn't rename to destination readme file: %v", err)
+	}
+}
+
+func mustWriteLine(w io.Writer, msg string) {
+	if _, err := w.Write([]byte(msg + "\n")); err != nil {
+		log.Fatal("Couldn't write %s: %v", msg, err)
+	}
+}
+
+// genManTreeFromOpts generates a man page for the command and all descendants.
+// The pages are written to the opts.Path directory.
+// This is a copy from cobra, but it will include Hidden commands.
+func genManTreeFromOpts(cmd *cobra.Command, header doc.GenManHeader, dir string) error {
+	for _, c := range cmd.Commands() {
+		if (!c.IsAvailableCommand() && !c.Hidden) || c.IsAdditionalHelpTopicCommand() {
+			continue
+		}
+		if err := genManTreeFromOpts(c, header, dir); err != nil {
+			return err
+		}
+	}
+
+	section := "1"
+	basename := strings.ReplaceAll(cmd.CommandPath(), " ", "-")
+	filename := filepath.Join(dir, basename+"."+section)
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return doc.GenMan(cmd, &header, f)
+}
+
+func getCmdsAndSystems(cmds []*cobra.Command) (user []*cobra.Command, system []*cobra.Command) {
+	for _, cmd := range cmds {
+		user = append(user, cmd)
+		user = append(user, collectSubCmds(cmd, false /* selectHidden */, false /* parentWasHidden */)...)
+	}
+	for _, cmd := range cmds {
+		system = append(system, collectSubCmds(cmd, true /* selectHidden */, false /* parentWasHidden */)...)
+	}
+
+	return user, system
+}
+
+// collectSubCmds get recursiverly commands from a root one.
+// It will filter hidden commands if selected, but will present children if needed.
+func collectSubCmds(cmd *cobra.Command, selectHidden, parentWasHidden bool) (cmds []*cobra.Command) {
+	for _, c := range cmd.Commands() {
+		if c.Name() == "help" {
+			continue
+		}
+		// Only continue selecting non hidden child of hidden commands
+		if (c.Hidden && !selectHidden) || (!c.Hidden && selectHidden && !parentWasHidden) {
+			continue
+		}
+		// Flip that we have a hidden parent
+		if c.Hidden && selectHidden {
+			parentWasHidden = true
+		}
+		cmds = append(cmds, c)
+		cmds = append(cmds, collectSubCmds(c, selectHidden, parentWasHidden)...)
+	}
+	return cmds
+}
+
+// filterCommandMarkdown filters SEE ALSO and add subindentation for commands
+// before writing to the writer.
+func filterCommandMarkdown(cmds []*cobra.Command, w io.Writer) {
+	pr, pw := io.Pipe()
+
+	go func() {
+		for _, cmd := range cmds {
+			if err := doc.GenMarkdown(cmd, pw); err != nil {
+				pw.CloseWithError(fmt.Errorf("Couldn't generate markdown for %s: %v", cmd.Name(), err))
+				return
+			}
+		}
+		pw.Close()
+	}()
+	scanner := bufio.NewScanner(pr)
+	var skip bool
+	for scanner.Scan() {
+		l := scanner.Text()
+		if strings.HasPrefix(l, "### SEE ALSO") || strings.Contains(l, "Auto generated by") {
+			skip = true
+		}
+		if strings.HasPrefix(l, "## ") {
+			skip = false
+		}
+		if skip {
+			continue
+		}
+		// Add 2 levels of subindentation
+		if strings.HasPrefix(l, "##") {
+			l = "##" + l
+		}
+		mustWriteLine(w, l)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Couldn't write generated markdown: %v", err)
+	}
+}
