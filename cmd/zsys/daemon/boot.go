912ea17bdd1f0cfcad92ce99b047659d38f00afc
Didier Roche <didrocks@ubuntu.com>
2019-10-31 08:15:21 +0100

    Boot cleanup: sync calls in a single file
    It's not used anywhere else now.

--- cmd/zsys/daemon/boot.go
+++ cmd/zsys/daemon/boot.go
@@ -3,8 +3,10 @@ package daemon
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/ubuntu/zsys/internal/config"
+	"github.com/ubuntu/zsys/internal/machines"
 	"github.com/ubuntu/zsys/internal/zfs"
 )
 
@@ -38,4 +40,29 @@ func syncBootPrepare() error {
 	}
 
 	return nil
+}
+
+// getMachines returns all scanned machines on the current system
+func getMachines(ctx context.Context, z *zfs.Zfs) (*machines.Machines, error) {
+	ds, err := z.Scan()
+	if err != nil {
+		return nil, err
+	}
+	cmdline, err := procCmdline()
+	if err != nil {
+		return nil, err
+	}
+	ms := machines.New(ctx, ds, cmdline)
+
+	return &ms, nil
+}
+
+// procCmdline returns kernel command line
+func procCmdline() (string, error) {
+	content, err := ioutil.ReadFile("/proc/cmdline")
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
 }
