7ecb95c90a9b107a0976def2d77a2b3f9bf08050
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2019-11-21 14:40:47 +0100

    Added polkit integration
    Added a struct authorizer to handle all the polkit operation.
    Added several actions for zsys and the corresponding policy file.
    
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

---
+++ internal/authorizer/authorizer.go
@@ -0,0 +1,181 @@
+package authorizer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/godbus/dbus"
+	"github.com/ubuntu/zsys/internal/log"
+)
+
+type caller interface {
+	Call(method string, flags dbus.Flags, args ...interface{}) *dbus.Call
+}
+
+// Authorizer is an abstrction of polkit authorization.
+type Authorizer struct {
+	authority caller
+	pid       uint32
+	uid       uint32
+
+	root string
+}
+
+func withAuthority(c caller) func(*Authorizer) {
+	return func(a *Authorizer) {
+		a.authority = c
+	}
+}
+
+func withRoot(root string) func(*Authorizer) {
+	return func(a *Authorizer) {
+		a.root = root
+	}
+}
+
+// New returns a new authorizer.
+func New(options ...func(*Authorizer)) (*Authorizer, error) {
+	bus, err := dbus.SystemBus()
+	if err != nil {
+		return nil, err
+	}
+	authority := bus.Object("org.freedesktop.PolicyKit1",
+		"/org/freedesktop/PolicyKit1/Authority")
+
+	a := Authorizer{
+		authority: authority,
+		root:      "/",
+	}
+
+	for _, option := range options {
+		option(&a)
+	}
+
+	return &a, nil
+}
+
+// Action is an polkit action
+type Action string
+
+const (
+	// ActionAlwaysAllowed is a no-op bypassing any user or dbus checks.
+	ActionAlwaysAllowed Action = "always-allowed"
+	// ActionManageService is the action to perform read operations.
+	ActionManageService Action = "com.ubuntu.zsys.manage-service"
+	// ActionSystemList is the action to perform system list operations.
+	ActionSystemList Action = "com.ubuntu.zsys.system-list"
+	// ActionSystemWrite is the action to perform system write operations.
+	ActionSystemWrite Action = "com.ubuntu.zsys.system-write"
+	// ActionUserWriteSelf is the action to perform user write operations on own user's datasets.
+	ActionUserWriteSelf Action = "com.ubuntu.zsys.user-write-self"
+	//ActionUserWriteOthers is the action to perform user operation on other user's datasets.
+	ActionUserWriteOthers Action = "com.ubuntu.zsys.user-write-others"
+)
+
+type polkitCheckFlags uint32
+
+const (
+	checkAllowInteration polkitCheckFlags = 1
+)
+
+type authSubject struct {
+	Kind    string
+	Details map[string]dbus.Variant
+}
+
+type authResult struct {
+	IsAuthorized bool
+	IsChallenge  bool
+	Details      map[string]string
+}
+
+// IsAllowed returns if the user is allowed to perform an operation.
+func (a Authorizer) IsAllowed(ctx context.Context, action Action, pid, uid uint32) bool {
+	if uid == 0 {
+		log.Debug(ctx, "Authorized as being administrator")
+		return true
+	} else if action == ActionAlwaysAllowed {
+		log.Debug(ctx, "Any user always authorized")
+		return true
+	}
+
+	f, err := os.Open(filepath.Join(a.root, fmt.Sprintf("proc/%d/stat", pid)))
+	if err != nil {
+		log.Errorf(ctx, "Couldn't open stat file for process: %v", err)
+		return false
+	}
+	defer f.Close()
+
+	startTime, err := getStartTimeFromReader(f)
+	if err != nil {
+		log.Errorf(ctx, "Couldn't determine start time of client process: %v", err)
+		return false
+	}
+
+	subject := authSubject{
+		Kind: "unix-process",
+		Details: map[string]dbus.Variant{
+			"pid":        dbus.MakeVariant(pid),
+			"start-time": dbus.MakeVariant(startTime),
+			"uid":        dbus.MakeVariant(uid),
+		},
+	}
+
+	var result authResult
+	err = a.authority.Call(
+		"org.freedesktop.PolicyKit1.Authority.CheckAuthorization", dbus.FlagAllowInteractiveAuthorization,
+		subject, string(action), subject.Details, checkAllowInteration, "").Store(&result)
+	if err != nil {
+		log.Errorf(ctx, "Call to polkit failed: %v", err)
+		return false
+	}
+
+	log.Debugf(ctx, "Polkit call result, authorized: %t", result.IsAuthorized)
+
+	return result.IsAuthorized
+}
+
+// getStartTimeFromReader determines the start time from a process stat file content
+//
+// The implementation is intended to be compatible with polkit:
+//    https://cgit.freedesktop.org/polkit/tree/src/polkit/polkitunixprocess.c
+func getStartTimeFromReader(r io.Reader) (uint64, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return 0, err
+	}
+	contents := string(data)
+
+	// start time is the token at index 19 after the '(process
+	// name)' entry - since only this field can contain the ')'
+	// character, search backwards for this to avoid malicious
+	// processes trying to fool us
+	//
+	// See proc(5) man page for a description of the
+	// /proc/[pid]/stat file format and the meaning of the
+	// starttime field.
+	idx := strings.IndexByte(contents, ')')
+	if idx < 0 {
+		return 0, errors.New("parsing error: missing )")
+	}
+	idx += 2 // skip ") "
+	if idx > len(contents) {
+		return 0, errors.New("parsing error: ) at the end")
+	}
+	tokens := strings.Split(contents[idx:], " ")
+	if len(tokens) < 20 {
+		return 0, errors.New("parsing error: less fields than required")
+	}
+	v, err := strconv.ParseUint(tokens[19], 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("parsing error: %v", err)
+	}
+	return v, nil
+}
