4fadcfe11066ea9b2f4fbb2add3bb21d7dc3c990
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2019-11-22 15:14:13 +0100

    Added copy util used by go generate
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

---
+++ internal/generators/copy.go
@@ -0,0 +1,41 @@
+// +build tools
+
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/ubuntu/zsys/internal/generators"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		log.Fatal("Expect 3 arguments: <source> <dest> <root_share>")
+	}
+	root := generators.DestDirectory(os.Args[3])
+	dir := filepath.Join(root, os.Args[2])
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		log.Fatalf("Couldn't create dest directory: %v", err)
+	}
+
+	from, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatalf("Couldn't open source file: %v", err)
+	}
+	defer from.Close()
+
+	dest, err := os.OpenFile(filepath.Join(dir, filepath.Base(os.Args[1])), os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal("Couldn't open dest file: %v", err)
+	}
+	defer dest.Close()
+
+	_, err = io.Copy(dest, from)
+	if err != nil {
+		log.Fatalf("Copy failed: %v", err)
+	}
+}
