4a9d69271e9a1c86d9271a27d93dd581550e5fe9
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2020-02-11 16:50:09 +0100

    code refactoring
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

--- internal/machines/export_test.go
+++ internal/machines/export_test.go
@@ -21,29 +21,29 @@ func (s SortedDatasets) Less(i, j int) bool {
 func (s SortedDatasets) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
 
 // Import from json to export the private fields
-func (m *Machines) UnmarshalJSON(b []byte) error {
+func (ms *Machines) UnmarshalJSON(b []byte) error {
 	mt := Machinesdump{}
 
 	if err := json.Unmarshal(b, &mt); err != nil {
 		return err
 	}
 
-	m.all = mt.All
-	m.cmdline = mt.Cmdline
-	m.current = mt.Current
-	m.nextState = mt.NextState
-	m.allSystemDatasets = mt.AllSystemDatasets
-	m.allUsersDatasets = mt.AllUsersDatasets
-	m.allPersistentDatasets = mt.AllPersistentDatasets
-	m.unmanagedDatasets = mt.UnmanagedDatasets
+	ms.all = mt.All
+	ms.cmdline = mt.Cmdline
+	ms.current = mt.Current
+	ms.nextState = mt.NextState
+	ms.allSystemDatasets = mt.AllSystemDatasets
+	ms.allUsersDatasets = mt.AllUsersDatasets
+	ms.allPersistentDatasets = mt.AllPersistentDatasets
+	ms.unmanagedDatasets = mt.UnmanagedDatasets
 
-	if m.current != nil {
+	if ms.current != nil {
 		for k, machine := range mt.All {
-			if machine.ID != m.current.ID {
+			if machine.ID != ms.current.ID {
 				continue
 			}
 			// restore current machine pointing to the same element than the hashmap
-			m.current = mt.All[k]
+			ms.current = mt.All[k]
 		}
 	}
 
@@ -51,16 +51,16 @@ func (m *Machines) UnmarshalJSON(b []byte) error {
 }
 
 // MakeComparable prepares Machines by resetting private fields that change at each invocation
-func (m *Machines) MakeComparable() {
-	ds := SortedDatasets(m.allSystemDatasets)
+func (ms *Machines) MakeComparable() {
+	ds := SortedDatasets(ms.allSystemDatasets)
 	sort.Sort(ds)
-	m.allSystemDatasets = ds
+	ms.allSystemDatasets = ds
 
-	m.z = nil
+	ms.z = nil
 }
 
 // SplitSnapshotName calls internal splitSnapshotName to split a snapshot name in base and id of a snapshot
 func SplitSnapshotName(s string) (string, string) { return splitSnapshotName(s) }
 
 // AllMachines exports machines lists for tests
-func (m *Machines) AllMachines() map[string]*Machine { return m.all }
+func (ms *Machines) AllMachines() map[string]*Machine { return ms.all }
