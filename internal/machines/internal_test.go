d8285457bbdbf2fccb6b7230a6bf4e8a648343f1
Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>
2020-03-09 16:06:54 +0100

    Tests for state.remove()
    Co-authored-by: Didier Roche <didrocks@ubuntu.com>

--- internal/machines/internal_test.go
+++ internal/machines/internal_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ubuntu/zsys/internal/config"
 	"github.com/ubuntu/zsys/internal/testutils"
 	"github.com/ubuntu/zsys/internal/zfs"
+	"github.com/ubuntu/zsys/internal/zfs/libzfs/mock"
 )
 
 func init() {
@@ -268,6 +269,128 @@ func TestParentSystemTest(t *testing.T) {
 	}
 }
 
+/*
+FIXME 2020-03-09: This case fails with the current data structure.
+Case of a user state attached to 2 different machines, one of the machine has a subdataset.
+In this case, the state with the same ID is added twice to allUsersDatasets.
+
+This is described by the following test definition:
+
+pools:
+  - name: rpool
+    datasets:
+    - name: ROOT
+    - name: ROOT/ubuntu_1234
+[...]
+    - name: ROOT/ubuntu_5678
+[...]
+    - name: ROOT/ubuntu_1234/opt
+[...]
+    - name: ROOT/ubuntu_5678/opt
+      origin: rpool/ROOT/ubuntu_1234/opt@snap2
+    - name: USERDATA
+      canmount: off
+    - name: USERDATA/user1_abcd
+[...]
+    - name: USERDATA/root_bcde
+    - name: USERDATA/root_bcde/subfor5678
+      mountpoint: /root/subfor5678
+      last_used: 2018-08-03T21:55:33+00:00
+      bootfs_datasets: rpool/ROOT/ubuntu_5678
+      snapshots:
+        - name: snap2
+          mountpoint: /root/subfor5678:inherited
+          canmount: on:local
+          creation_time: 2019-04-18T02:45:55+00:00
+
+*/
+func TestRemoveInternal(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		stateName     string
+		linkedStateID string
+
+		setPropertyErr bool
+
+		wantErr bool
+		isNoOp  bool
+	}{
+		"Initial state":                                 {}, // This state doesn’t call remove at all but is used to compare golden files
+		"Remove user state unconditionnally":            {stateName: "rpool/USERDATA/user2_2222"},
+		"Remove user state conditionnally":              {stateName: "rpool/USERDATA/user2_2222", linkedStateID: "rpool/ROOT/ubuntu_5678"},
+		"Don't remove user state on bad state id match": {stateName: "rpool/USERDATA/user2_2222", linkedStateID: "doesnt match"},
+
+		"Remove user state unconditionnally linked to 2 states":                   {stateName: "rpool/USERDATA/user3_3333"},
+		"Remove user state unconditionnally linked to 2 states and its snapshots": {stateName: "rpool/USERDATA/root_bcde"},
+		"Unassociate user state linked to one state":                              {stateName: "rpool/USERDATA/user3_3333", linkedStateID: "rpool/ROOT/ubuntu_1234"},
+
+		"Remove user snapshot state":                                    {stateName: "rpool/USERDATA/root_bcde@snaproot1"},
+		"Remove user snapshot state (linked to system state: no check)": {stateName: "rpool/USERDATA/root_bcde@snap2"},
+		"Remove user snapshot clone state":                              {stateName: "rpool/USERDATA/user4_clone"},
+
+		"Remove system state without user datasets": {stateName: "rpool/ROOT/ubuntu_6789"},
+		"Remove system state and its user datasets": {stateName: "rpool/ROOT/ubuntu_5678"},
+
+		// Error on clones from state
+		"Error on removing state with state clone":   {stateName: "rpool/USERDATA/user4_for_state_clone", wantErr: true},
+		"Error on removing state with dataset clone": {stateName: "rpool/USERDATA/user5_for_manual_clone", wantErr: true},
+
+		"Revert unassociate user state if we get an error":          {stateName: "rpool/USERDATA/user2_2222", linkedStateID: "rpool/ROOT/ubuntu_5678", setPropertyErr: true, wantErr: true, isNoOp: true},
+		"Don’t destroy system state if user remove issues an error": {stateName: "rpool/ROOT/ubuntu_5678", setPropertyErr: true, wantErr: true, isNoOp: true},
+	}
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			dir, cleanup := testutils.TempDir(t)
+			defer cleanup()
+
+			libzfs := testutils.GetMockZFS(t)
+			fPools := testutils.NewFakePools(t, filepath.Join("testdata", "state_remove_internal.yaml"), testutils.WithLibZFS(libzfs))
+			defer fPools.Create(dir)()
+
+			lzfs := libzfs.(*mock.LibZFS)
+			lzfs.ErrOnSetProperty(tc.setPropertyErr)
+
+			ms, err := New(context.Background(), "", WithLibZFS(libzfs))
+			if err != nil {
+				t.Error("expected success but got an error scanning for machines", err)
+			}
+
+			initMachines := ms.CopyForTests(t)
+
+			// Create init file for comparison
+			if tc.stateName == "" {
+				assertMachinesToGolden(t, ms)
+				return
+			}
+
+			s := ms.getStateFromName(t, tc.stateName)
+
+			err = s.remove(context.Background(), ms.z, tc.linkedStateID)
+			if err != nil {
+				if !tc.wantErr {
+					t.Fatalf("expected no error but got: %v", err)
+				}
+				return
+			}
+			if err == nil && tc.wantErr {
+				t.Fatal("expected an error but got none")
+			}
+
+			got, err := New(context.Background(), "", WithLibZFS(libzfs))
+			if err != nil {
+				t.Error("expected success but got an error scanning for machines", err)
+			}
+			if tc.isNoOp {
+				assertMachinesEquals(t, initMachines, got)
+				return
+			}
+
+			assertMachinesToGolden(t, got)
+		})
+	}
+}
+
 // assertDatasetsOrigin compares got maps of origin to reference files, based on test name.
 func assertDatasetsOrigin(t *testing.T, got map[string]*string) {
 	want := make(map[string]*string)
@@ -275,6 +398,31 @@ func assertDatasetsOrigin(t *testing.T, got map[string]*string) {
 
 	if diff := cmp.Diff(want, got, cmpopts.EquateEmpty()); diff != "" {
 		t.Errorf("Dataset origin mismatch (-want +got):\n%s", diff)
+	}
+}
+
+// assertMachinesToGolden compares got slice of machines to reference files, based on test name.
+func assertMachinesToGolden(t *testing.T, got Machines) {
+	t.Helper()
+
+	want := Machines{}
+	got.MakeComparable()
+	testutils.LoadFromGoldenFile(t, got, &want)
+
+	assertMachinesEquals(t, want, got)
+}
+
+// assertMachinesEquals compares two machines
+func assertMachinesEquals(t *testing.T, m1, m2 Machines) {
+	t.Helper()
+
+	m1.MakeComparable()
+	m2.MakeComparable()
+
+	if diff := cmp.Diff(m1, m2, cmpopts.EquateEmpty(),
+		cmp.AllowUnexported(Machines{}),
+		cmpopts.IgnoreUnexported(zfs.Dataset{}, zfs.DatasetProp{})); diff != "" {
+		t.Errorf("Machines mismatch (-want +got):\n%s", diff)
 	}
 }
 
