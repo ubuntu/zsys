74a484b6d72c4ab7c1a23eb9460badd0d9105c69
Didier Roche <didrocks@ubuntu.com>
2020-04-07 12:21:25 +0200

    Test case for some user dataset clones without association kept
    Co-authored-by: Jean-Baptiste Lallement <jean-baptiste@ubuntu.com>

--- internal/machines/machines_test.go
+++ internal/machines/machines_test.go
@@ -1167,6 +1167,7 @@ func TestGC(t *testing.T) {
 		"Remove user clone state with subdataset":                   {def: "gc_system_with_users_clone_subdataset.yaml"},
 		"Don't remove user clone state with snapshot on it kept":    {def: "gc_system_with_users_clone_with_manual_snapshot.yaml", isNoOp: true},
 		"Don't remove user clone state with snapshot on subdataset": {def: "gc_system_with_users_clone_subdataset_with_manual_snapshot.yaml", isNoOp: true},
+		"Ensure user clones are accounted by policy":                {def: "gc_system_with_users_with_untagged_clones.yaml"},
 		// FIXME: user1_clone should be removed once TestNew is fixed (attaching the clone, and so its snapshots to the system state indirectly)
 		//"Remove unassociated user clone after deleting its snapshot": {def: "gc_system_with_users_clone_with_auto_snapshot.yaml"},
 		"Remove unassociated user clone after deleting its snapshot which was linked to system state": {def: "gc_system_with_users_clone_with_auto_snapshot_attached_to_system_state.yaml"},
